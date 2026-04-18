cbcd96bf9b4d82e93512030a712115606e30cffc
Rahul Gorai <rahulgorai0206@gmail.com>
2026-04-18 12:41:27 +0530

    Update response message in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('My Second Successfull Build');
+  res.send('My automated build.');
 });
 
 app.listen(port, () => {
